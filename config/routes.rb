a3886ee99c271d599990e101c7ed482c06634c01
Kutomore <kutomore@gmail.com>
2020-05-01 16:59:13 -0300

    Improving frontend

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@
 Rails.application.routes.draw do
   resources :profiles
   resources :artists
+  resources :genres
   resources :events
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   root to: 'events#index'
 end
